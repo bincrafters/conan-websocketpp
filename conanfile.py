27876a11e635bb143d576ac5dac79f5253ff0c83
Jerry W <jerrywiltse@gmail.com>
2017-09-25 19:50:25 -0400

    working version

--- conanfile.py
+++ conanfile.py
@@ -8,19 +8,18 @@ class WebsocketPPConan(ConanFile):
     license = "https://github.com/zaphoyd/websocketpp/blob/master/COPYING"
     requires =  "OpenSSL/1.0.2l@conan/stable", \
             "zlib/1.2.8@conan/stable", \
-            "Boost.Random/1.64.0@bincrafters/stable", \
-            "Boost.System/1.64.0@bincrafters/stable", \
-            "Boost.Thread/1.64.0@bincrafters/stable", \
+            "Boost.Random/1.64.0@bincrafters/testing", \
+            "Boost.System/1.64.0@bincrafters/testing", \
+            "Boost.Thread/1.64.0@bincrafters/testing", \
             "Boost.Asio/1.64.0@bincrafters/testing", \
                       
     def source(self):
         source_url = "https://github.com/zaphoyd/websocketpp"
-        tools.get("{0}/{1}/archive/v{2}.tar.gz".format(source_url, self.name, self.version))
+        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
 
     def package(self):
-        for lib_short_name in self.lib_short_names:
-            include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)		
+        include_dir = os.path.join("{0}-{1}".format(self.name, self.version), self.name)
+        self.copy(pattern="*", dst="include", src=include_dir)		
 
     def package_id(self):
         self.info.header_only()
