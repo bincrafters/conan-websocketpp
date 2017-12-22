2369d112f41672b51606ffaf48e6444cefea398d
Jerry W <jerrywiltse@gmail.com>
2017-12-22 10:38:42 -0500

    package refresh testing

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,8 @@
-from conans import ConanFile, tools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
+from conans import ConanFile, tools
 
 class WebsocketPPConan(ConanFile):
     name = "websocketpp"
@@ -7,20 +10,25 @@ class WebsocketPPConan(ConanFile):
     url = "https://github.com/bincrafters/conan-websocketpp"
     description = "Header only C++ library that implements RFC6455 The WebSocket Protocol"
     license = "https://github.com/zaphoyd/websocketpp/blob/master/COPYING"
-    requires =  "OpenSSL/1.0.2l@conan/stable", \
-            "zlib/1.2.8@conan/stable", \
-            "Boost.Random/1.64.0@bincrafters/stable", \
-            "Boost.System/1.64.0@bincrafters/stable", \
-            "Boost.Thread/1.64.0@bincrafters/stable", \
-            "Boost.Asio/1.64.0@bincrafters/stable", \
+    source_subfolder = "sources"
+    requires =  (
+        "OpenSSL/[>=1.0.2l]@conan/stable", 
+        "zlib/[>=1.2.8]@conan/stable", 
+        "Boost.Random/[>=1.64.0]@bincrafters/stable", 
+        "Boost.System/[>=1.64.0]@bincrafters/stable", 
+        "Boost.Thread/[>=1.64.0]@bincrafters/stable", 
+        "Boost.Asio/[>=1.64.0]@bincrafters/stable"
+    )
                       
     def source(self):
+        archive_name = "{0}-{1}".format(self.name, self.version)
         source_url = "https://github.com/zaphoyd/websocketpp"
         tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
+        os.rename(archive_name, self.source_subfolder)
 
     def package(self):
-        include_dir = os.path.join("{0}-{1}".format(self.name, self.version), self.name)
-        self.copy(pattern="*", dst="include", src=include_dir)		
+        include_dir = os.path.join(self.source_subfolder, "websocketpp")
+        self.copy(pattern="*", dst="websocketpp", src=include_dir)
 
     def package_id(self):
         self.info.header_only()
