6d8f9b78c5dd02105e6a72bcaec7a6acf4e9dd85
Jerry W <jerrywiltse@gmail.com>
2017-12-29 19:21:05 -0500

    try docker and adjust get_module_url

--- build.py
+++ build.py
@@ -18,10 +18,13 @@ def get_module_filename():
     return get_module_name() + ".py"
     
 def get_module_url():
-    return get_module_location() + "/" + get_module_name() + ".py"
+    return get_module_location() + "/" + get_module_filename()
 
 if __name__ == "__main__":
+    
     tools.download(get_module_url(), get_module_filename(), overwrite=True)
+    
     module = importlib.import_module(get_module_name())
+    
     module.build()
     
