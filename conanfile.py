7b80e8f1f64be4b198a36b572c35127ef562708c
SSE4 <tomskside@gmail.com>
2018-02-18 00:38:51 +0700

    - build via CMake (ensures websocketpp-config.cmake is generated)

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from conans import ConanFile, tools
+from conans import ConanFile, tools, CMake
 
 
 class WebsocketPPConan(ConanFile):
@@ -12,14 +12,17 @@ class WebsocketPPConan(ConanFile):
     description = "Header only C++ library that implements RFC6455 The WebSocket Protocol"
     license = "BSD 3-Clause"
     source_subfolder = "source_subfolder"
-    requires = (
-        "OpenSSL/1.0.2l@conan/stable",
-        "zlib/1.2.11@conan/stable",
-        "boost_random/1.66.0@bincrafters/stable",
-        "boost_system/1.66.0@bincrafters/stable",
-        "boost_thread/1.66.0@bincrafters/stable",
-        "boost_asio/1.66.0@bincrafters/stable"
-    )
+    exports_sources = ["CMakeLists.txt"]
+    generators = ['cmake']
+    settings = "os", "arch", "compiler", "build_type"
+
+    def requirements(self):
+        self.requires.add('OpenSSL/1.0.2l@conan/stable')
+        self.requires.add('zlib/1.2.11@conan/stable')
+        self.requires.add('boost_random/1.66.0@bincrafters/stable')
+        self.requires.add('boost_system/1.66.0@bincrafters/stable')
+        self.requires.add('boost_thread/1.66.0@bincrafters/stable')
+        self.requires.add('boost_asio/1.66.0@bincrafters/stable')
                       
     def source(self):
         archive_name = "{0}-{1}".format(self.name, self.version)
@@ -27,12 +30,19 @@ class WebsocketPPConan(ConanFile):
         tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
         os.rename(archive_name, self.source_subfolder)
 
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions['BUILD_TESTS'] = False
+        cmake.definitions['BUILD_EXAMPLES'] = False
+        cmake.configure()
+        cmake.build()
+        cmake.install()
+
     def package(self):
-        src_include_dir = os.path.join(self.source_subfolder, "websocketpp")
-        dst_include_dir = os.path.join("include", "websocketpp")
-        
         self.copy(pattern="COPYING", dst="license", src=self.source_subfolder)
-        self.copy(pattern="*", dst=dst_include_dir, src=src_include_dir)
+
+    def package_info(self):
+        self.cpp_info.builddirs.append(os.path.join(self.package_folder, 'cmake'))
 
     def package_id(self):
         self.info.header_only()
